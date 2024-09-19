40c84583eb521c599d80c3f09b1066f7c043a24b
danieloselu3 <doselu3@gmail.com>
2024-09-19 20:26:03 +0300

    added the patient model

---
+++ dags/dbt/urban_umbrella/models/refined/ref_patient.sql
@@ -0,0 +1,38 @@
+with source_data as (
+    SELECT *
+    FROM {{ ref('stg_medical_receipt_data') }}
+), 
+norm_dt as (
+    SELECT
+        patient_id,
+        patient_nm,
+        birth_dt,
+        gender_cd,
+        phone_num,
+        email_id
+    FROM source_data
+),
+default_value as (
+    SELECT
+        'Missing' as receipt_id,
+        'Missing' as encounter_id,
+        'Missing' as patient_id,
+),
+with_default_value as (
+    SELECT * FROM norm_dt
+    UNION ALL
+    SELECT * FROM default_value
+),
+hashed_data as (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['receipt_id']) }} as sgk_medical_receipt_id,
+        {{ dbt_utils.generate_surrogate_key([
+            'receipt_id',
+            'encounter_id',
+            'patient_id',
+        ]) }} as sgk_medical_receipt_diff,
+        *,
+        '{{ run_started_at }}'::timestamptz as load_dts
+    FROM with_default_value
+)
+SELECT * FROM hashed_data
