5ef43886c0d0749e63b84e5f48c3b2f2a231fc6e
danieloselu3 <doselu3@gmail.com>
2024-09-20 14:03:02 +0300

    added provider model in the refined and delivery layer

--- dags/dbt/urban_umbrella/models/refined/ref_provider.sql
+++ dags/dbt/urban_umbrella/models/refined/ref_provider.sql
@@ -4,32 +4,23 @@ with source_data as (
 ), 
 norm_dt as (
     SELECT
+        provider_npi_num,
         provider_nm,
         provider_specialty_nm,
-        provider_npi_num
+        record_source_nm
     FROM source_data
-),
-default_value as (
-    SELECT
-        'Missing' as receipt_id,
-        'Missing' as encounter_id,
-        'Missing' as patient_id,
-),
-with_default_value as (
-    SELECT * FROM norm_dt
-    UNION ALL
-    SELECT * FROM default_value
 ),
 hashed_data as (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['receipt_id']) }} as sgk_medical_receipt_id,
+        {{ dbt_utils.generate_surrogate_key(['provider_npi_num']) }} as sgk_provider_id,
         {{ dbt_utils.generate_surrogate_key([
-            'receipt_id',
-            'encounter_id',
-            'patient_id',
-        ]) }} as sgk_medical_receipt_diff,
+            'provider_npi_num',
+            'provider_nm',
+            'provider_specialty_nm',
+            'record_source_nm'
+        ]) }} as sgk_provider_diff,
         *,
         '{{ run_started_at }}'::timestamptz as load_dts
-    FROM with_default_value
+    FROM norm_dt
 )
 SELECT * FROM hashed_data
